9be9db75357e173e64355d796e9937ebcf7b382b
Bantibhai007 <166740682+Bantibhai007@users.noreply.github.com>
2024-04-28 16:56:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "7041642612:AAE-G8Sk-0Dwd-wdFacfbz2OCBjLoUbOCYA",
+             bot_token= "7127570280:AAH--1rp4AVyyYWlGJnxfCEjvgh5wCVVPUc",
              api_id=24630940,
              api_hash= "df46be1ad6b0027ec1dff6dd3bb703dd")
 
